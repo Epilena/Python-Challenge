9983a4b94bac84629d67b849381608e9aa9e51bb
Celena Blackwood <cblackwoodmph@gmail.com>
2021-10-08 19:01:59 -0500

    completed coding of Pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,14 +3,20 @@ import os
 
 #import module for reading CSV files
 import csv
-from typing import Counter
+#from typing import Counter
 
-file= os.path.join ("PyBank", 'Resources', 'budget_data.csv')
+file= os.path.join ('PyBank', 'Resources', 'budget_data.csv')
 
 # initializing the titles and rows list
 Months = []
 Amount = []
 Change = []
+
+#creation of additional variable
+Total_months= 0
+Dec_profit = 0
+Inc_profit = 0
+
 
 #read the CSV file
 with open (file, 'r') as csvfile:
@@ -18,45 +24,49 @@ with open (file, 'r') as csvfile:
     #create a csv reader object
     csvreader = csv.reader(csvfile, delimiter =',')
     header=next(csvreader)
-    print(header)
+    #print(header)
 
 
 #extract each data row:
     for row in csvreader:
         Months.append(row[0])
         Amount.append(int(row[1]))
-    for i in range(len(Amount)-1):
-        Change.append(Amount[i+1]-Amount[i])
-        #print(Months)
+    for i in range(1, len(Amount)):
+        Change.append((int(Amount[i])-int(Amount[i-1])))
 
-#Total Months
-    Total_Months = len(Months)
-    print (Total_Months)
+#total Months
+    Total_months = len(Months)
+    #print(Total_months)
+#Max and Min in changes of profit/losses
+    Dec_profit= min(Change)
+    Inc_profit= max(Change)
+#Pull indexes of Max and Min
+    Mon_inc= Change.index(max(Change))+1
+    Mon_dec = Change.index(min(Change))+1
 
-#Greatest increase and decrease in Profits
-    Dec_Profit= min(Amount)
-    Inc_Profit = max(Amount)
-    print(Dec_Profit, Inc_Profit)
+#print data to terminal
+print(f"Financial Analysis")
+print("----------------------------------")
+print(f"Total Months: {len(list(Months))}")
+print(f"Total: ${sum(Amount)}")
+print(f"Average Change: ${round(sum(Change)/len(Change), 2)}")
+print(f"Greatest Increase in Profits: {Months[Mon_inc]} (${(Inc_profit)})")
+print(f"Greatest Decrease in Profits: {Months[Mon_dec]} (${(Dec_profit)})")
 
-    #calcluate total volume 
-
-    Total_vol =sum(int(row[1])
-    print(Total_vol)
-    #Total_Volume = 0
-    #profit =0
-    #loss = 0
-
-    #Amount = int(row[1])
-    #if Amount >=0:
-            #profit= profit + Amount 
-    #elif Amount <0:
-           # loss = loss + Amount
-    #Total_volume = profit - loss
-    
-    #print(Total_volume)
-        
-
-    #Average Change Calculation
-        ## calculate total change/change in inputs
-
-    
+#output to a text file
+output = os.path.join ("Pybank", 'output.txt')
+with open(output, "w") as new:
+   new.write("Financial Analysis")
+   new.write("\n")
+   new.write("----------------------------------")
+   new.write("\n")
+   new.write(f"Total Months: {len(Months)}")
+   new.write("\n")
+   new.write(f"Total: ${sum(Amount)}")
+   new.write("\n")
+   new.write(f"Average Change: ${round(sum(Change)/len(Change), 2)}")
+   new.write("\n")
+   new.write(f"Greatest Increase in Profits: {Months[Mon_inc]} (${(Inc_profit)})")
+   new.write("\n")
+   new.write(f"Greatest Decrease in Profits: {Months[Mon_dec]} (${(Dec_profit)})")
+ 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,23 +8,50 @@ import csv
 elect_data = os.path.join("PyPoll","Resources", "election_data.csv")
 
 #read the CSV file
-with open (elect_data, 'r') as csvfile:
+with open (elect_data) as csvfile:
 
     #create a csv reader object
-    csvreader = csv.reader(csvfile)
+    csvreader = csv.reader(csvfile) 
     
-    csv_header = next(csvfile)
+    csvheader = next(csvreader)
 
     # initializing the titles and rows list
-    Voters_candidates = []
-    Voters_per_candidate = []
+    Voters_ID = []
+    County = []
+    Candidate = []
+    Votes = 0
+    CandidateVotes={}
 
-    #extract field names, first row
-    fields = next(csvreader)
-    #extract each data row:
+    #read through each data row:
     for row in csvreader:
-        Voters_candidates.append(row)
+        #total vote count
+        Votes = Votes+1
+        #candidate voted for
+        candidate_name= row[2]
+        #create loop to tally candidate voted for
+        if candidate_name not in CandidateVotes:
+            CandidateVotes[candidate_name] = 0
+            #Candidate.append(candidate_name)
+    
+        #add votes to candidate's total count
+            CandidateVotes[candidate_name]+=1
+        #CandidateVotes[candidate_name]+=1
 
+    for candidate_name in CandidateVotes:
+        result= CandidateVotes[candidate_name]
+        vote_perc = float(result)/float(Votes) *100
+        cand_res= (f"{candidate_name}: {vote_perc:.2f}% ({result:,})")
 
+    #determining overall winner
+        
+            
+#print results
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {Votes}")
+print("--------------------------")
+print(cand_res)
+print("---------------------------")
+#print(f"Winner: {}")
+#print("---------------------------")
 
-print()
